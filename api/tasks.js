175b5a048d7adc1e39a096f121597c281913cd5c
Artistkent <collins.ose5@gmail.com>
2026-06-08 11:38:34 +0100

    Deleted useless serverless functions

--- api/tasks.js
+++
@@ -1,7 +0,0 @@
-import { neon } from '@neondatabase/serverless';
-
-export default async function handler(req, res) {
-  const sql = neon(process.env.DATABASE_URL);
-  const tasks = await sql`SELECT * FROM gantt_chart ORDER BY id`;
-  res.status(200).json(tasks);
-}
